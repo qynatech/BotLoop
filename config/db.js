fa0972b6ecc0b5c58017167f6bb1ea52529bae34
qynatech <husnanur068@gmail.com>
2026-05-09 20:59:09 +0700

    fix db connection

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+const db = mysql.createConnection(process.env.MYSQL_URL || {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("DB error: ", err);
-    } else {
-        console.log("DB Connected");
-    }
+  if (err) {
+    console.log("DB error: ", err);
+  } else {
+    console.log("DB Connected");
+  }
 });
 
 module.exports = db;
